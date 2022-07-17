c81ae25ebc0ffda032ad5d3b1b168fec55e954be
ReetVirk <85968565+ReetVirk@users.noreply.github.com>
2022-07-17 16:21:54 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,64 @@
+function showTcl() {
+
+    tcl = document.getElementById("tcl_div");
+    if (tcl.style.display === "block") {
+      tcl.style.display = "none";
+    } 
+    
+    else {
+      tcl.style.display = "block";
+    }
+
+}
+
+function showAgggtm() {
+
+    agggtm = document.getElementById("agggtm_div");
+    if (agggtm.style.display === "block") {
+      agggtm.style.display = "none";
+    } 
+    
+    else {
+      agggtm.style.display = "block";
+    }
+
+}
+
+function showTgib() {
+
+    tgib = document.getElementById("tgib_div");
+    if (tgib.style.display === "block") {
+      tgib.style.display = "none";
+    } 
+    
+    else {
+      tgib.style.display = "block";
+    }
+
+}
+
+function showCoss() {
+
+  coss = document.getElementById("coss_div");
+  if (coss.style.display === "block") {
+    coss.style.display = "none";
+  } 
+  
+  else {
+    coss.style.display = "block";
+  }
+
+}
+
+function showTsz() {
+
+  coss = document.getElementById("tsz_div");
+  if (coss.style.display === "block") {
+    coss.style.display = "none";
+  } 
+  
+  else {
+    coss.style.display = "block";
+  }
+
+}
